35c70334fb84cd610589b714ee9ba5ae3bfb38ff
AvaCado <eliserichardsson@gmail.com>
2022-05-06 19:52:39 -0400

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,21 +11,21 @@ pool.connect(() => {
   console.log('connected to the db 🌈');
 });
 
+const queryString = `SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2`;
 const cohort = process.argv[2];
-const limit = process.argv[3];
+const limit = process.argv[3] || 5;
+const values = [`%${cohort}%`, limit];
 
 pool
-  .query(
-    `SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-        FROM students
-        JOIN cohorts ON cohorts.id = cohort_id
-        WHERE cohorts.name LIKE '%${cohort}%'
-        LIMIT ${limit};`
-  )
+  .query(queryString, values)
   .then((res) => {
-    res.rows.forEach((user) => {
+    res.rows.forEach((rows) => {
       console.log(
-        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+        `${rows.name} has an id of ${rows.student_id} and was in the ${rows.cohort} cohort`
       );
     });
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,18 +11,18 @@ pool.connect(() => {
   console.log('connected to the db 🌈');
 });
 
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
 const cohortName = process.argv[2];
+const values = [cohortName];
 
 pool
-  .query(
-    `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM teachers
-    JOIN assistance_requests ON teachers.id = teacher_id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name = '${cohortName}'
-    ORDER BY teacher;`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(`${row.cohort} : ${row.teacher}`);
